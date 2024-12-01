fd881ee5e8fe843b38f46259cbc66896faf07a05
TatRendo <tatisrendon3@gmail.com>
2024-11-30 22:43:50 -0500

    alll

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -8,16 +8,13 @@ public class Program
         Address address1 = new Address("35-46 alcala St", "Bogotá", "Cundinamarca", "Colombia");
         Address address2 = new Address("16-09 La gaviota St", "Ibague", "Tolima", "Colombia");
 
-        // Create customers
         Customer customer1 = new Customer("Tatiana Rendon", address1);
         Customer customer2 = new Customer("Kleithin Alvarez", address2);
 
-        // Create products
         Product product1 = new Product("Empanadas", "OR001", 16m, 2);
         Product product2 = new Product("Ice cream: cokes and cream", "OR002", 10m, 2);
         Product product3 = new Product("Coca-Cola", "OR003", 10m, 1);
 
-        // Create orders
         Order order1 = new Order(customer1);
         order1.AddProduct(product1);
         order1.AddProduct(product2);
@@ -25,7 +22,6 @@ public class Program
         Order order2 = new Order(customer2);
         order2.AddProduct(product3);
 
-        // Display order details
         DisplayOrder(order1);
         DisplayOrder(order2);
     }
