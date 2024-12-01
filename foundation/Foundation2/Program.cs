d1c4af19b249e45e09b836a4f2c9a7b17c5c7b47
TatRendo <tatisrendon3@gmail.com>
2024-11-30 22:12:42 -0500

    2all

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -6,27 +6,23 @@ public class Program
     public static void Main(string[] args)
     {
         // Crear videos
-        Video video1 = new Video("Learn C# Basics", "Programming Guru", 600);
-        Video video2 = new Video("Advanced C# Techniques", "Code Master", 1200);
-        Video video3 = new Video("C# for Beginners", "Tech Educator", 900);
+        Video video1 = new Video("How to Train Your Dragon", "second movie", 600);
+        Video video2 = new Video("Arcane", "Stromae & Pomme - Ma Meilleure Ennemier", 1200);
+        Video video3 = new Video("general conference 2024", "October", 900);
 
-        // Agregar comentarios al video1
-        video1.AddComment(new Comment("Alice", "This video was super helpful!"));
-        video1.AddComment(new Comment("Bob", "Great explanation, thanks!"));
-        video1.AddComment(new Comment("Charlie", "Could you cover more examples?"));
+        video1.AddComment(new Comment("Leonardo", "I can't get over Estoico's death."));
+        video1.AddComment(new Comment("Kleithin", "excellent character development."));
+        
+        video2.AddComment(new Comment("Erika", "the Arcane soundtrack when Ekko and Jinx dance is beautiful!"));
+        video2.AddComment(new Comment("Eivy", "impossible love but possible enemies."));
+        
 
-        // Agregar comentarios al video2
-        video2.AddComment(new Comment("Dave", "This is exactly what I needed."));
-        video2.AddComment(new Comment("Eve", "Clear and concise."));
+        video3.AddComment(new Comment("Jonathan", "God's favourite... It made me cry when I remembered that I am still a beloved son of God."));
+        video3.AddComment(new Comment("Dante", "Burying Our Weapons of Rebellion. It doesn't matter what weapons they are, if they do harm and distance us from God we have to bury the weapons."));
+        video3.AddComment(new Comment("Danny", "Ye are my friends. Sometimes we make decisions but if we are firm we can become good friends and find eternal friends."));
 
-        // Agregar comentarios al video3
-        video3.AddComment(new Comment("Frank", "Good for beginners."));
-        video3.AddComment(new Comment("Grace", "Loved the pacing of this tutorial!"));
-
-        // Lista de videos
         List<Video> videos = new List<Video> { video1, video2, video3 };
 
-        // Mostrar información de cada video
         foreach (var video in videos)
         {
             video.DisplayInfo();
