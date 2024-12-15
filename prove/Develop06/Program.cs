a033bdb4daa065a1c370e32934ef9e121146728e
Tatiana Rendon <155861527+TatRendo@users.noreply.github.com>
2024-12-14 22:37:13 -0500

    Update Program.cs

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -1,9 +1,10 @@
-using System;
+var manager = new GoalManager();
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop06 World!");
-    }
-}
+manager.AddGoal(new SimpleGoal("Run a marathon", 1000));
+manager.AddGoal(new EternalGoal("Read Scriptures", 100));
+manager.AddGoal(new ChecklistGoal("Attend the temple", 50, 10, 500));
+
+manager.RecordProgress("Read Scriptures");
+manager.RecordProgress("Attend the temple");
+
+manager.DisplayGoals();
